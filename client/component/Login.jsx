e3ccea654e9d41c0184dafa5d9f70ceb4aae54a8
Alexander James Young <ayoungj@umich.edu>
2020-06-26 10:18:40 -0400

    created sructure for input fields and buttons

--- client/component/Login.jsx
+++ client/component/Login.jsx
@@ -2,7 +2,23 @@ import React, { Component } from "react";
 
 class Login extends Component {
   render() {
-    return <div>LOGIN</div>;
+    return (
+      <main>
+        <h1>Login</h1>
+        <form id='loginForm'>
+          <div>
+            <input>
+            </input>
+          </div>
+            <input>
+            </input>
+          <div>
+            <button form='loginForm'>
+            </button>
+          </div>
+        </form>
+      </main>
+    )
   }
 }
 
