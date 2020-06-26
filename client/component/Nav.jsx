906abba3a71cada78163ae124c9e5f1962c2c1ed
cupofjoy <eelantung@gmail.com>
2020-06-25 22:48:37 -0400

    boiler plate and temp nav bar for routing

---
+++ client/component/Nav.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <Link to="/register">REGISTER</Link>
+          </li>
+          <li>
+            <Link to="/login ">LOGIN</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
