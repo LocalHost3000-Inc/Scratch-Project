f71c1fc340a1c88bb4123007e2f4869eed824479
Alexander James Young <ayoungj@umich.edu>
2020-06-26 19:28:04 -0400

    created buttons

--- client/component/Header.jsx
+++ client/component/Header.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import "../styles/header.scss";
 
 export default class Header extends Component {
@@ -8,10 +10,18 @@ export default class Header extends Component {
   render() {
     return (
       <header className="header">
-        <div className="headerInnerDiv">
+        <div >
           <span className="spanOne">Local</span>
           <span className="spanTwo">Host</span>
           <span className="spanThree"> 3000</span>
+        </div>
+        <div className="buttonsDiv">
+          <button className='searchButton'>
+            <p>Search</p>
+          </button>
+          <button>
+            <p>Profile</p>
+          </button>
         </div>
       </header>
     );
