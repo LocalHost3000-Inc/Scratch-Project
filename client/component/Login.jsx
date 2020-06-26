765c080c846c9dda823a9e5c4084e4c2fa9474d9
Alexander James Young <ayoungj@umich.edu>
2020-06-26 11:01:34 -0400

    set up login to alter state based on whether loggin was sucessful, laid out fetch reqeust

--- client/component/Login.jsx
+++ client/component/Login.jsx
@@ -5,7 +5,8 @@ class Login extends Component {
     super();
     this.state = {
       usernameInputField: '',
-      passwordInputField: ''
+      passwordInputField: '',
+      errorMessage: ''
     }
     this.handleUsernameInputField = this.handleUsernameInputField.bind(this);
     this.handlePasswordInputField = this.handlePasswordInputField.bind(this);
@@ -19,8 +20,26 @@ class Login extends Component {
   }
   handleSubmitButton(e) {
     console.log('call to submit button')
+    let url = 'http://localhost:8080/api/login';
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: this.state.usernameInputField,
+        password: this.state.passwordInputField,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.props.addCurrentUser(data);
+        this.props.history.push('/');
+      })
+      .catch((err) => {
+        console.warn(err);
+        this.setState({errorMessage: 'sorry, we could not process your username and password'})
+        
+      });
     this.setState({usernameInputField: '', passwordInputField: ''});
-    
     e.preventDefault();
   }
   render() {
@@ -38,6 +57,9 @@ class Login extends Component {
             <button form='loginForm' type='submit'  value='Submit' onClick={this.handleSubmitButton}>
               Submit
             </button>
+          </div>
+          <div>
+            <p>{this.state.errorMessage}</p>
           </div>
         </form>
       </main>
