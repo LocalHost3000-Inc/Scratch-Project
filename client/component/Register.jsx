45cefc83e0a11e841207bf40cce78458ba492f41
cupofjoy <eelantung@gmail.com>
2020-06-26 10:17:46 -0400

    updated route for register

--- client/component/Register.jsx
+++ client/component/Register.jsx
@@ -32,7 +32,7 @@ class Register extends Component {
     e.preventDefault();
 
     if (this.state.password === this.state.confirmPassword) {
-      fetch("http://localhost:8080/api/users", {
+      fetch("http://localhost:8080/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
