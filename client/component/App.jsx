ec8cb360b57fc85589dae54a9df59abb12d369c0
cupofjoy <eelantung@gmail.com>
2020-06-26 10:04:20 -0400

    registration

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Register from "./Register.jsx";
 import Profile from "./Profile.jsx";
 import Home from "./Home.jsx";
