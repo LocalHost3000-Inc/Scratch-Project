7a4b301ff98b03204036db16fb6a1c0dd195f7a2
cupofjoy <eelantung@gmail.com>
2020-06-30 21:33:48 -0400

    changed List to functional component

--- client/component/List.jsx
+++ client/component/List.jsx
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import Item from './Item.jsx';
 
-class List extends Component {
-  // iterate thru this.props.searchResults
-  // with each iteration we want to render an Item component
-  // we'll want to include JSX attributes on each Item component so the Item component can render a piece of the user object
-  // some JSX attributes will be
-
-  render() {
-    const items = [];
-    for (let user of this.props.searchResults) {
-      items.push(
-        <Item key={user.id} username={user.username} name={user.name} email={user.email} home={user.home} />
-      );
-    }
-    return <div className="container">{items}</div>;
+const List = (props) => {
+  function renderResults(searchResults) {
+    return searchResults.map(user => <Item key={user.id} username={user.username} name={user.name} email={user.email} home={user.home} />)
   }
+
+  return <div className="container">{renderResults(props.searchResults)}</div>;
 }
 
 export default List;
